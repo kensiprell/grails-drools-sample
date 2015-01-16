8406d45e8d254df4d45c5e4e60267e459d715010
Ken Siprell <ken.siprell@gmail.com>
2015-01-16 20:43:39 +0100

    fix database log error

---
+++ src/groovy/grails/plugin/drools_sample/ImprovedH2Dialect.groovy
@@ -0,0 +1,15 @@
+package grails.plugin.drools_sample
+
+import org.hibernate.dialect.H2Dialect;
+
+public class ImprovedH2Dialect extends H2Dialect {
+	@Override
+	public String getDropSequenceString(String sequenceName) {
+		return "drop sequence if exists " + sequenceName;
+	}
+
+	@Override
+	public boolean dropConstraints() {
+		return false;
+	}
+}
