e0fc9513ce963c698aef39831c48f8292ea3630d
Ken Siprell <ken.siprell@gmail.com>
2015-04-26 18:02:20 +0200

    add war troubleshooting log.error

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,11 +2,18 @@ import grails.plugin.drools_sample.DroolsRule
 
 class BootStrap {
 	def init = { servletContext ->
-		def classLoader = new GroovyClassLoader()
+		def classLoader = this.class.classLoader
+		def applicationRules = DroolsRule.findAllByPackageName("application")
+		log.error "BOOTSTRAP before applicationRules: ${applicationRules.size()}"
+		log.error "BOOTSTRAP before DroolsRule: ${DroolsRule.getAll().size()}"
 		String drlText = classLoader.getResourceAsStream("drools-rules/application/application.drl").text
 		new DroolsRule(rule: drlText, description: "application.drl", packageName: "application").save(flush: true)
 		drlText = classLoader.getResourceAsStream("drools-rules/ticket/ticket.drl").text
 		new DroolsRule(rule: drlText, description: "ticket.drl", packageName: "ticket").save(flush: true)
+		applicationRules = DroolsRule.findAllByPackageName("application")
+		log.error "BOOTSTRAP after applicationRules: ${applicationRules.size()}"
+		log.error "BOOTSTRAP after DroolsRule: ${DroolsRule.getAll().size()}"
+		log.error "BLANK LINE"
 	}
 
 	def destroy = {
